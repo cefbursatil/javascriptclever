fd3be164dfc7d910f5dbe1f31e2b5cdceb557b96
Carlos Forero <cefbursatil@gmail.com>
2021-08-18 15:28:07 -0500

    Events and DOM

--- js/main.js
+++ js/main.js
@@ -45,39 +45,44 @@ class moneda{
         this.ValorDolares=pValorDolares;
     }
 }
+let dolaresArray=[];
+if(localStorage.getItem('Dolares')!== null){
+    dolaresArray=localStorage.getItem('Dolares');
+}
+function CalcConversion(){
+    let moneda1="usd";
+    let moneda2="usd";
+    let cantMoneda1=0;
+    let outputText="";
+    let convArray=[];
 
+    moneda1=document.getElementById("curr1").value;
+    cantMoneda1=document.getElementById("cantidad").value;
+    moneda2=document.getElementById("curr2").value;
+    //Creo los objetos
+    mon1 = new moneda(moneda1,cantMoneda1);
+    mon2 = new moneda(moneda2,0);
+
+    let conv = new conversor(mon1,mon2);
+    //guardo conversor en array para analizar estadisticas
+    convArray.push(conv);
+
+    // calculo la cantidad de la segunda moneda
+    // texto de salida y cantDolares
+    outputText=conv.output();
+    const resultOutput = document.getElementById("result");
+    resultOutput.innerText=conv.Moneda2.CantidadMoneda;
+    dolaresArray.push(convArray[convArray.length-1].cantDolares);
+    localStorage.SetItem('Dolares',dolaresArray);
+}
 
 
 // Inicializo las variables
-let moneda1="usd";
-let moneda2="usd";
-let cantMoneda1=0;
-let outputText="";
-let convArray=[];
-let dolaresArray=[];
+let moneda1prub=document.getElementById("curr1").value;
 let mayorDolares=0;
 // Pido las monedas y su correspondiente cantidad
-while(moneda1 !="esc"){
-    moneda1=prompt("¿que moneda quieres convertir? (ej: USD) \n (esc si no quieres convertir nada)");
-    if(moneda1!="esc"){
-        cantMoneda1=parseFloat(prompt("¿Que cantidad quieres convertir? \n (ej: 10000)"));
-        moneda2=prompt("¿a cual moneda quieres convertir convertir? (ej: AUD)");
-        //Creo los objetos
-        mon1 = new moneda(moneda1,cantMoneda1);
-        mon2 = new moneda(moneda2,0);
-
-        let conv = new conversor(mon1,mon2);
-        //guardo conversor en array para analizar estadisticas
-        convArray.push(conv);
-
-        // calculo la cantidad de la segunda moneda
-        // texto de salida y cantDolares
-        outputText=conv.output();
-        dolaresArray.push(convArray[convArray.length-1].cantDolares);
-        // alerta al usuario
-        alert(outputText);
-    }
-}
+const convertidor=document.getElementById("convert");
+convertidor.addEventListener('click',CalcConversion);
 //ordenamos array para capturar el mayor de las conversiones y ofrecemos alerta final
 mayorDolares=dolaresArray.sort(function(a, b){return b-a})[0];
-alert("Realizaste "+convArray.length+ " conversiones de moneda \n y la mayor cantidad de dolares en una conversion fue "+mayorDolares);
+//alert("Realizaste "+convArray.length+ " conversiones de moneda \n y la mayor cantidad de dolares en una conversion fue "+mayorDolares);
